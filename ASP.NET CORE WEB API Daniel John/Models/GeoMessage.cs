0193d2a23fafbe77e76d373fbc1db311666270f6
Daniel Persson <daniel.persson18@learnet.se>
2021-04-30 09:39:07 +0200

    Lagt till nuget packages

---
+++ ASP.NET CORE WEB API Daniel John/Models/GeoMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_WEB_API_Daniel_John.Models
+{
+    public class GeoMessage
+    {
+        public string message { get; set; }
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+    }
+}
