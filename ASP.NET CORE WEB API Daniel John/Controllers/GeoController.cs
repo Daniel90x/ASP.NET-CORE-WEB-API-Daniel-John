d8d1c96c34fac10240f3deacb0252936613678a9
John <john.a.nilsson@hotmail.com>
2021-04-30 11:22:07 +0200

    Börjat på controller

---
+++ ASP.NET CORE WEB API Daniel John/Controllers/GeoController.cs
@@ -0,0 +1,31 @@
+﻿using ASP.NET_CORE_WEB_API_Daniel_John.Data;
+using ASP.NET_CORE_WEB_API_Daniel_John.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_WEB_API_Daniel_John.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoController : ControllerBase
+    {
+        private readonly GeoDbContext _context;
+
+        public GeoController(GeoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<GeoMessage>> GetGeo(int id)
+        {
+            var test = await _context.GeoMessage.FindAsync(id);
+            return test;
+        }
+
+    }
+}
