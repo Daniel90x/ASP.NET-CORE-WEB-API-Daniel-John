8324a1e4398bd241959b555f96c165915396fd4b
John <john.a.nilsson@hotmail.com>
2021-05-03 11:18:35 +0200

    .

---
+++ ASP.NET CORE WEB API Daniel John/Models/MyUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_WEB_API_Daniel_John.Models
+{
+    public class MyUser
+    {
+    }
+}
