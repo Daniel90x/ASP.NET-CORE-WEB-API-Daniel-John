6a1b489bd2e8b4e7f09f60fe445569e57ae7c455
John <john.a.nilsson@hotmail.com>
2021-05-05 11:54:52 +0200

    Lunch commit

--- ASP.NET CORE WEB API Daniel John/Controllers/GeoController.cs
+++ ASP.NET CORE WEB API Daniel John/Controllers/GeoController.cs
@@ -18,7 +18,7 @@ namespace ASP.NET_CORE_WEB_API_Daniel_John.Controllers
     {
         [ApiController]
         [ApiVersion("1.0")]
-        [Route("api/v{Version:ApiVersion}/[controller]-comments")] //[Version:ApiVersion]
+        [Route("api/v{Version:ApiVersion}/[controller]-comments")]
         public class GeoController : ControllerBase
         {
             private readonly GeoDbContext _context;
@@ -62,5 +62,64 @@ namespace ASP.NET_CORE_WEB_API_Daniel_John.Controllers
             }
 
         }
+    }
+
+
+    namespace v2
+    {
+
+        [ApiController]
+        [ApiVersion("2.0")]
+        [Route("api/v{Version:ApiVersion}/[controller]-comments")]
+        public class GeoController : ControllerBase
+        {
+            private readonly GeoDbContext _context;
+
+            public GeoController(GeoDbContext context)
+            {
+                _context = context;
+            }
+
+            [HttpGet("{id}")]
+            public async Task<ActionResult<GeoMessage>> GetGeo(int id)
+            {
+                var test = await _context.GeoMessage.FindAsync(id);
+                if (test == null)
+                {
+                    return NotFound();
+                }
+                return test;
+            }
+
+
+            [HttpGet]
+            public async Task<ActionResult<IEnumerable<GeoMessageDTO>>> GetGeoAll()
+            {
+
+
+                return await _context.GeoMessage.Select(g => g.ToDto()).ToListAsync();
+            }
+
+            [Authorize]
+            [HttpPost]
+            public async Task<ActionResult<GeoMessageDTO>> PostGeoMessage(GeoMessageDTO geoPost)
+            {
+
+                var geo = geoPost.GeoMessageModel();
+                _context.GeoMessage.Add(geo);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+
+            }
+
+        }
+
+
+
+
+
+
+
     }
 }
